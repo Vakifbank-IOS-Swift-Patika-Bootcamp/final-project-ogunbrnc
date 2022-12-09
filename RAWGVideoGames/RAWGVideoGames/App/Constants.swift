ea1a2fd9d247bcfbdca6617726d09449170aa1da
Ogün Birinci <olgunogunbirinci@gmail.com>
2022-12-09 15:51:15 +0300

    create task for get request

---
+++ RAWGVideoGames/RAWGVideoGames/App/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  RAWGVideoGames
+//
+//  Created by Ogün Birinci on 9.12.2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "712c03cf7bbb4cffb5e7a94a0e901e07"
+}
---
+++ RAWGVideoGames/RAWGVideoGames/Core/Client.swift
@@ -0,0 +1,38 @@
+//
+//  Client.swift
+//  RAWGVideoGames
+//
+//  Created by Ogün Birinci on 9.12.2022.
+//
+
+import Foundation
+
+final class Client {
+    
+    
+    @discardableResult
+    class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+            }
+        }
+        task.resume()
+
+        return task
+    }
+}
