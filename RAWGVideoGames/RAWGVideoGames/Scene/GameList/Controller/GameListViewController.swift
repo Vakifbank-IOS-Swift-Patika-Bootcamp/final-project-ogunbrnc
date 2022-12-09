7a863b1a9ff8519feff1c08ff0e779511c0b9463
Ogün Birinci <olgunogunbirinci@gmail.com>
2022-12-09 14:55:38 +0300

    game list screen added

---
+++ RAWGVideoGames/RAWGVideoGames/Scene/GameList/Controller/GameListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  GameListViewController.swift
+//  RAWGVideoGames
+//
+//  Created by Ogün Birinci on 9.12.2022.
+//
+
+import UIKit
+
+final class GameListViewController: UIViewController {
+    
+    @IBOutlet weak var gameListCollectionView: UICollectionView! {
+        didSet {
+            gameListCollectionView.delegate = self
+            gameListCollectionView.dataSource = self
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+extension GameListViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as UICollectionViewCell
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfCellsInRow = 2
+
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+
+        let totalSpace = flowLayout.sectionInset.left
+        + flowLayout.sectionInset.right
+        + (flowLayout.minimumInteritemSpacing * CGFloat(numberOfCellsInRow - 1))
+
+        let size = Int((collectionView.bounds.width - totalSpace) / CGFloat(numberOfCellsInRow))
+
+        return CGSize(width: size, height: size)
+    }
+    
+}
+
