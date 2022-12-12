64333c19906b52d193c7484db02da71c5ca09f15
Ogun Birinci <olgunogunbirinci@gmail.com>
2022-12-13 00:44:47 +0300

    create local notification manager

---
+++ RAWGVideoGames/RAWGVideoGames/Core/LocalNotificationManager.swift
@@ -0,0 +1,71 @@
+//
+//  localNotificationManager.swift
+//  RAWGVideoGames
+//
+//  Created by Ogün Birinci on 12.12.2022.
+//
+
+import Foundation
+import UserNotifications
+
+enum VoidResult {
+    case success
+    case failure(Error)
+}
+enum LocalNotificationManagerError: Error {
+    case notAuthorized
+}
+
+extension LocalNotificationManagerError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .notAuthorized:
+            return "To use this feature you must enable notifications in settings".localized()
+        }
+    }
+}
+
+protocol LocalNotificationManagerProtocol {
+    func scheduleLocalNotification(title: String, message: String, date: Date, completion: @escaping (VoidResult) -> Void)
+}
+
+final class LocalNotificationManager: LocalNotificationManagerProtocol {
+    static let shared = LocalNotificationManager()
+    private let notificationCenter: UNUserNotificationCenter!
+    
+    private init() {
+        notificationCenter = UNUserNotificationCenter.current()
+    }
+    
+    func scheduleLocalNotification(title: String, message: String, date: Date, completion: @escaping (VoidResult) -> Void) {
+        notificationCenter.getNotificationSettings { (settings) in
+            DispatchQueue.main.async
+            {
+                if(settings.authorizationStatus == .authorized)
+                {
+                    let content = UNMutableNotificationContent()
+                    content.title = title
+                    content.body = message
+                    
+                    let dateComp = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+                    
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComp, repeats: false)
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    
+                    self.notificationCenter.add(request) { (error) in
+                        if error != nil {
+                            completion(.failure(error!))
+                            print("Error " + error.debugDescription)
+                            return
+                        }
+                    }
+                    completion(.success)
+                }
+                else
+                {
+                    completion(.failure(LocalNotificationManagerError.notAuthorized))
+                }
+            }
+        }
+    }
+}
