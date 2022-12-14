6e96dc59f5baedd53e84f775ca319443566ddcf7
Ogun Birinci <olgunogunbirinci@gmail.com>
2022-12-14 13:23:29 +0300

    add game time information to game list tableview cell

--- RAWGVideoGames/RAWGVideoGames/Scene/GameList/View/GameListTableViewCell.swift
+++ RAWGVideoGames/RAWGVideoGames/Scene/GameList/View/GameListTableViewCell.swift
@@ -39,22 +39,56 @@ class GameListTableViewCell: UITableViewCell {
         return label
     }()
     
+    private let gameTimeIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "timer")
+        imageView.tintColor = .label
+
+        return imageView
+    }()
+    
+    private let gameTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textColor = .label
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        return label
+    }()
+    
 
     
     // MARK: Configure UI Components
        private func configureConstraints() {
+           
            let gameImageViewConstraints = [
                 gameImageView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 20),
                 gameImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
-                gameImageView.widthAnchor.constraint(equalToConstant: 175),
+                gameImageView.widthAnchor.constraint(equalToConstant: 175 ),
                 gameImageView.heightAnchor.constraint(equalToConstant: 175)
            ]
            
            let gameNameLabelConstraints = [
                 gameNameLabel.topAnchor.constraint(equalTo: gameImageView.topAnchor),
-                gameNameLabel.leadingAnchor.constraint(equalTo: gameImageView.trailingAnchor, constant: 20)
+                gameNameLabel.leadingAnchor.constraint(equalTo: gameImageView.trailingAnchor, constant: 20),
+                gameNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20)
+
            ]
           
+           let gameTimeIconImageViewConstraints = [
+                gameTimeIconImageView.leadingAnchor.constraint(equalTo: gameNameLabel.leadingAnchor),
+                gameTimeIconImageView.topAnchor.constraint(equalTo: gameNameLabel.bottomAnchor,constant: 10),
+                gameTimeIconImageView.widthAnchor.constraint(equalToConstant: 16),
+                gameTimeIconImageView.heightAnchor.constraint(equalToConstant: 16)
+           ]
+           
+           let gameTimeLabelConstraints = [
+                gameTimeLabel.topAnchor.constraint(equalTo: gameTimeIconImageView.topAnchor),
+                gameTimeLabel.leadingAnchor.constraint(equalTo: gameTimeIconImageView.trailingAnchor, constant: 5),
+           ]
+          
+           
            let gamePlatformLabelConstraints = [
                 gamePlatformLabel.leadingAnchor.constraint(equalTo: gameImageView.leadingAnchor),
                 gamePlatformLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
@@ -65,6 +99,8 @@ class GameListTableViewCell: UITableViewCell {
            NSLayoutConstraint.activate(gameImageViewConstraints)
            NSLayoutConstraint.activate(gameNameLabelConstraints)
            NSLayoutConstraint.activate(gamePlatformLabelConstraints)
+           NSLayoutConstraint.activate(gameTimeIconImageViewConstraints)
+           NSLayoutConstraint.activate(gameTimeLabelConstraints)
 
          
 
@@ -75,13 +111,16 @@ class GameListTableViewCell: UITableViewCell {
         addSubview(gameImageView)
         addSubview(gameNameLabel)
         addSubview(gamePlatformLabel)
-            
+        addSubview(gameTimeIconImageView)
+        addSubview(gameTimeLabel)
     }
     
     // If there is no image, the "photo" image will be used to provide a consistent structure
     func configureCell(game: GameModel){
         gameNameLabel.text = game.name
+        gameTimeLabel.text = String(game.playtime ?? 0)
         gameImageView.sd_setImage(with: URL(string: game.imageURL ?? ""))
+        
         
         let platforms = game.platforms?.map { $0.platform.name }.joined(separator: ",") ?? ""
         gamePlatformLabel.text = "Platforms:".localized() + platforms
