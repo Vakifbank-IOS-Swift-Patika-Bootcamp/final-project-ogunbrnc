1255c18d00cadd593596f9b5c95539e8f77a036f
Ogun Birinci <olgunogunbirinci@gmail.com>
2022-12-16 02:25:55 +0300

    add testGetGameImageURL to GameDetailViewModelUnitTest

---
+++ RAWGVideoGames/RAWGVideoGamesTests/GameDetailViewModelUnitTest.swift
@@ -0,0 +1,40 @@
+//
+//  GameDetailViewModelUnitTest.swift
+//  RAWGVideoGamesTests
+//
+//  Created by Ogün Birinci on 16.12.2022.
+//
+
+import XCTest
+
+final class GameDetailViewModelUnitTest: XCTestCase {
+
+    var viewModel: GameDetailViewModel!
+    var fetchExpectation: XCTestExpectation!
+    
+    override func setUpWithError() throws {
+        viewModel = GameDetailViewModel()
+        viewModel.delegate = self
+        fetchExpectation = expectation(description: "fetchGames")
+    }
+    
+    func testGetGameImageURL() {
+        //Given
+        XCTAssertEqual(viewModel.getGameImageURL(),nil)
+        
+        //When
+        viewModel.fetchGameDetail(id: 3498)
+        waitForExpectations(timeout: 10)
+        
+        let imageURL = URL(string: "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg")
+        //Then
+        XCTAssertEqual(viewModel.getGameImageURL(),imageURL)
+    }
+
+}
+
+extension GameDetailViewModelUnitTest: GameDetailViewModelDelegate {
+    func gameLoaded() {
+        fetchExpectation.fulfill()
+    }
+}
