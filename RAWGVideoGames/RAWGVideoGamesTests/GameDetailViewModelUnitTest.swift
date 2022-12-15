f46fe119f6f646614261e64f4c3e156c00c58217
Ogun Birinci <olgunogunbirinci@gmail.com>
2022-12-16 02:36:30 +0300

    add testGetGameDescription to GameDetailViewModelUnitTest

--- RAWGVideoGames/RAWGVideoGamesTests/GameDetailViewModelUnitTest.swift
+++ RAWGVideoGames/RAWGVideoGamesTests/GameDetailViewModelUnitTest.swift
@@ -90,6 +90,20 @@ final class GameDetailViewModelUnitTest: XCTestCase {
         //Then
         XCTAssertEqual(viewModel.getGameTag(), "Singleplayer,Steam Achievements,Multiplayer,Atmospheric,Full controller support")
     }
+    
+    func testGetGameDescription() {
+        //Given
+        XCTAssertEqual(viewModel.getGameDescription(),"")
+        
+        //When
+        viewModel.fetchGameDetail(id: 3498)
+        waitForExpectations(timeout: 10)
+        
+        let description = "Rockstar Games went bigger, since their previous installment of the series. You get the complicated and realistic world-building from Liberty City of GTA4 in the setting of lively and diverse Los Santos, from an old fan favorite GTA San Andreas. 561 different vehicles (including every transport you can operate) and the amount is rising with every update. \nSimultaneous storytelling from three unique perspectives: \nFollow Michael, ex-criminal living his life of leisure away from the past, Franklin, a kid that seeks the better future, and Trevor, the exact past Michael is trying to run away from. \nGTA Online will provide a lot of additional challenge even for the experienced players, coming fresh from the story mode. Now you will have other players around that can help you just as likely as ruin your mission. Every GTA mechanic up to date can be experienced by players through the unique customizable character, and community content paired with the leveling system tends to keep everyone busy and engaged."
+
+        //Then
+        XCTAssertEqual(viewModel.getGameDescription(), description)
+    }
 }
 
 extension GameDetailViewModelUnitTest: GameDetailViewModelDelegate {
