33b565979a1d4eee19d4cb6cbe9e3a9016d0895f
Ogun Birinci <olgunogunbirinci@gmail.com>
2022-12-16 00:00:32 +0300

    add testGetGameCount function for GameListViewModelDelegate

--- RAWGVideoGames/RAWGVideoGames/Scene/GameDetail/Controller/GameDetailViewController.swift
+++ RAWGVideoGames/RAWGVideoGames/Scene/GameDetail/Controller/GameDetailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class GameDetailViewController: BaseViewController {
     
--- RAWGVideoGames/RAWGVideoGamesTests/RAWGVideoGamesTests.swift
+++ RAWGVideoGames/RAWGVideoGamesTests/RAWGVideoGamesTests.swift
@@ -6,31 +6,35 @@
 //
 
 import XCTest
-@testable import RAWGVideoGames
 
-final class RAWGVideoGamesTests: XCTestCase {
+final class GameListViewModelUnitTest: XCTestCase {
 
+    var viewModel: GameListViewModel!
+    var fetchExpectation: XCTestExpectation!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = GameListViewModel()
+        viewModel.delegate = self
+        fetchExpectation = expectation(description: "fetchGames")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testGetGameCount() throws {
+        //Given
+        XCTAssertEqual(viewModel.getGameCount(), 0)
+        
+        //When
+        viewModel.fetchGames()
+        waitForExpectations(timeout: 10)
+        
+        //Then
+        XCTAssertEqual(viewModel.getGameCount(), 20)
     }
+    
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+}
+
+extension GameListViewModelUnitTest: GameListViewModelDelegate {
+    func gamesLoaded() {
+        fetchExpectation.fulfill()
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
